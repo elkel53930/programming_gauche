95b681ac138fb1517efd4d2adca15daccf4491c7
Kazuki Iida <elkel53930@gmail.com>
2018-05-09 19:00:51 +0900

    p.84

--- ch07_procedure/chap07.scm
+++ ch07_procedure/chap07.scm
@@ -68,3 +68,59 @@
   (cond [(null? args) '()]
         [(null? (cdr args)) (car args)]
         [else (append2 (car args) (apply append (cdr args)))]))
+
+(use util.match)
+
+(define (append . args)
+  (match args
+      [ () '()]
+      [ (a) a ]
+      [ (a . b) (append2 a (apply append b))]))
+
+(define (max-number lis)
+  (match lis
+    [() (error "max-number needs at least one number")]
+    [(x . y) (fold (lambda (a b) (if (> a b) a b)) x y)]))
+
+(define (mit-form->primitive-form expr)
+  (match expr
+    [('define (func. args) . body)  ;; defineは名前自体にマッチ
+     (list 'define func (list* 'lambda args body))]))
+
+(define (primitive-form->mit-form expr)
+  (match expr
+    [('define func ('lambda args . body))
+     (list* 'define (cons func args) body)]))
+
+(define (make-list num . args)
+  (define (maker n init)
+    (if (= n 0)
+        '()
+        (cons init (maker (- n 1) init))))
+  (maker num (if (null? args) #f (car args))))
+
+(define (make-list num . args)
+  (let-optionals* args ((init #f))
+    (define (maker n)
+      (if (= n 0) '() (cons init (maker (- n 1)))))
+    (maker num)))
+
+(define (person . args)
+  (let-keywords args ((name "Anonymous")
+                      (age  "unknown"))
+    (print name " is " age " year(s) old.")))
+
+(person :name "Kevin" :age 20)
+(person :name "Kevin")
+(person :age 20)
+(person :age 20 :name "Kevin")
+
+
+(define (person . args)
+  (let-keywords args ((name "Anonymous")
+                      (age  "unknown")
+                      . other-args)
+    (print name " is " age " year(s) old.")
+    (print "Other info: " other-args)))
+
+(person :abc 'ABC :born-in "Hawaii")
